266c3100b0e300cb5e299316b1bc2875a920c79c
TommyViriyaIrawan <167950147+TommyViriyaIrawan@users.noreply.github.com>
2024-11-27 14:24:48 +0700

    update harga menu

--- VDKitchen/app/Http/Controllers/HomeController.php
+++ VDKitchen/app/Http/Controllers/HomeController.php
@@ -12,67 +12,67 @@ class HomeController extends Controller
         $foods = [
             [
                 'name' => 'Nasi Tim Ayam',
-                'price' => 'Rp',
+                'price' => 'Rp24.000',
                 'image' => 'menu1.jpg', // Tambahkan gambar di folder public
             ],
             [
                 'name' => 'Mie Ayam',
-                'price' => 'Rp',
+                'price' => 'Rp19.000',
                 'image' => 'menu2.jpg',
             ],
             [
                 'name' => 'Mie Ayam Yamin',
-                'price' => 'Rp',
+                'price' => 'Rp20.000',
                 'image' => 'menu2.jpg',
             ],
             [
                 'name' => 'Mie Kangkung Ayam',
-                'price' => 'Rp',
+                'price' => 'Rp24.000',
                 'image' => 'menu2.jpg',
             ],
             [
                 'name' => 'Bihun Ayam',
-                'price' => 'Rp',
+                'price' => 'Rp19.000',
                 'image' => 'menu2.jpg',
             ],
             [
                 'name' => 'Bihun Ayam Yamin',
-                'price' => 'Rp',
+                'price' => 'Rp20.000',
                 'image' => 'menu2.jpg',
             ],
             [
                 'name' => 'Bihun Kangkung Ayam',
-                'price' => 'Rp',
+                'price' => 'Rp24.000',
                 'image' => 'menu2.jpg',
             ],
             [
                 'name' => 'Kwetiau Ayam',
-                'price' => 'Rp',
+                'price' => 'Rp19.000',
                 'image' => 'menu2.jpg',
             ],
             [
                 'name' => 'Kwetiau Ayam Yamin',
-                'price' => 'Rp',
+                'price' => 'Rp20.000',
                 'image' => 'menu2.jpg',
             ],
             [
                 'name' => 'Kwetiau Kangkung Ayam',
-                'price' => 'Rp',
+                'price' => 'Rp24.000',
                 'image' => 'menu2.jpg',
             ],
             [
                 'name' => 'Locupan Ayam',
-                'price' => 'Rp',
+                'price' => 'Rp22.000',
                 'image' => 'menu2.jpg',
             ],
             [
                 'name' => 'Baso Ikan/Sapi (isi 5)',
-                'price' => 'Rp',
+                'price' => 'Rp16.000',
                 'image' => 'menu2.jpg',
             ],
             [
                 'name' => 'Pangsit Ayam Rebus',
-                'price' => 'Rp',
+                'price' => 'Rp16.000',
                 'image' => 'menu2.jpg',
             ],
         ];
@@ -80,47 +80,47 @@ class HomeController extends Controller
         $drinks = [
             [
                 'name' => 'Teh Tawar',
-                'price' => 'Rp',
+                'price' => 'Rp1.000',
                 'image' => 'menu2.jpg',
             ],
             [
                 'name' => 'Es Teh Tawar',
-                'price' => 'Rp',
+                'price' => 'Rp2.000',
                 'image' => 'menu2.jpg',
             ],
             [
                 'name' => 'Teh Manis',
-                'price' => 'Rp',
+                'price' => 'Rp2.000',
                 'image' => 'menu2.jpg',
             ],
             [
                 'name' => 'Es Teh Manis',
-                'price' => 'Rp',
+                'price' => 'Rp3.000',
                 'image' => 'menu2.jpg',
             ],
             [
                 'name' => 'Kopi',
-                'price' => 'Rp',
+                'price' => 'Rp5.000',
                 'image' => 'menu2.jpg',
             ],
             [
                 'name' => 'Teh Pucuk',
-                'price' => 'Rp',
+                'price' => 'Rp5.000',
                 'image' => 'menu2.jpg',
             ],
             [
                 'name' => 'Aqua Botol',
-                'price' => 'Rp',
+                'price' => 'Rp5.000',
                 'image' => 'menu2.jpg',
             ],
             [
                 'name' => 'Liang Teh',
-                'price' => 'Rp',
+                'price' => 'Rp12.000',
                 'image' => 'menu2.jpg',
             ],
             [
                 'name' => 'Minuman Badak',
-                'price' => 'Rp',
+                'price' => 'Rp15.000',
                 'image' => 'menu2.jpg',
             ],
         ];
